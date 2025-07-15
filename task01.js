629f0a55900ce645ba69bdaac8b64871b43ea685
Ruksit <ruksitm@gmail.com>
2025-07-15 16:03:16 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -1,29 +1,45 @@
 "use strict";
 
-/* 
+/*
 Task 01: Greet a Person Using Different Function Declaration Methods
 
 📝 Instructions
 - Create a function that accepts one parameter: name
 - The function should return a string in this format: "Hello, <name>!"
     For example: Fn("John Doe") returns "Hello, John Doe!"
-    
+
 - Implement this using all three function declaration types:
   - Function Declaration
   - Function Expression
   - Arrow Function
 
 ❌ Do not print inside the function (no console.log).
-✅ Only return the greeting string. 
+✅ Only return the greeting string.
 */
 
 // 👨‍💻 TODO-01: Function Declaration: greetWithDeclaration(name)
+function greetWithDeclaration(name) {
+  return `Hello, ${name}!`;
+}
 
 // 👨‍💻 TODO-02: Function Expression: greetWithExpression(name)
+const greetWithExpression = function(name){
+  return "Hello, " + name + "!";
+}
 
 // 👨‍💻 TODO-03: Arrow Function: greetWithArrow(name)
+// Sol.1
+// const greetWithArrow = (name)=>{
+//   return "Hello, " + name + "!";
+// }
+
+
+//Sol.2 Short hand for one line
+const greetWithArrow = (name) => "Hello, " + name + "!";
 
 // Example Test Cases
 console.log(greetWithDeclaration("John Doe")); // "Hello, John Doe!"
 console.log(greetWithExpression("John Doe")); // "Hello, John Doe!"
 console.log(greetWithArrow("John Doe")); // "Hello, John Doe!"
+
+//-m "task01 solution"
